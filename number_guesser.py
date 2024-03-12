4073b2e0993d5d989a168eb9a2ee8d1b863d455e
waithium <160033656+waithium@users.noreply.github.com>
2024-03-12 05:37:48 +0530

    Update number_guesser.py
    added comments to the file

--- number_guesser.py
+++ number_guesser.py
@@ -1,10 +1,13 @@
 # a random number guessing game
+#importing random module
 import random
 
+#generating answer for one particular session
 ans = random.randrange(6)
 num = ''
 tries = 0
 
+#the game
 while ans != num:
     if tries < 3:
         num = int(input('guess the number (hint: 0-5): '))
